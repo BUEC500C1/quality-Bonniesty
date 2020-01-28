1d482b4ac74e4898fd8c3e2e9eaa378bb2a23eef
Bonniesty <43017800+Bonniesty@users.noreply.github.com>
2020-01-28 17:10:20 -0500

    add test cases

--- test_arabic_to_roman.py
+++ test_arabic_to_roman.py
@@ -12,6 +12,19 @@ import pytest
 def test_case():
     assert test.arabic2roman(3) == "III"
     assert test.arabic2roman(1) == "I"
+    assert test.arabic2roman(10) == "X"
+    assert test.arabic2roman(9) == "IX"
+    assert test.arabic2roman(40) == "XL"
+    assert test.arabic2roman(1000) == "M"
+    assert test.arabic2roman(100) == "C"
+    assert test.arabic2roman(400) == "CD"
+    assert test.arabic2roman(500) == "D"
+    assert test.arabic2roman(5) == "V"
+    assert test.arabic2roman(4) == "IV"
+    assert test.arabic2roman(7) == "VII"
+    assert test.arabic2roman(2013) == "MMXIII"
+    assert test.arabic2roman(1975) == "MCMLXXV"
+    assert test.arabic2roman(3999) == "MMMCMXCIX"    
     
     
 if __name__ == "__main__":
